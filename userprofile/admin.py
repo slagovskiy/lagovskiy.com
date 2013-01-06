da6c691a01de1e952a7b81a806035ac72a881e83
Sergey Lagovskiy <slagovskiy@gmail.com>
2013-01-07 02:56:34 +0700

    fix admin model for userprofile

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from userprofile.models import UserProfile
 
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user')
-    search_fields = ('user')
+    list_display = ('user',)
+    search_fields = ('user',)
     ordering = ('user',)
 
 admin.site.register(UserProfile, UserProfileAdmin)
