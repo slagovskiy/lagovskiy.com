faa0e48ec83402c518d29f719707dcf12eeb4920
Sergey Lagovskiy <slagovskiy@gmail.com>
2013-03-20 11:34:56 +0700

    add utils fro statistic

---
+++ apps/statistic/utils.py
@@ -0,0 +1,19 @@
+import re
+
+IP_RE = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+
+def get_ip(request):
+
+    ip_address = request.META.get('HTTP_X_FORWARDED_FOR',
+                                  request.META.get('REMOTE_ADDR', '127.0.0.1'))
+    if ip_address:
+        try:
+            ip_address = IP_RE.match(ip_address)
+            if ip_address:
+                ip_address = ip_address.group(0)
+            else:
+                ip_address = '10.0.0.1'
+        except IndexError:
+            pass
+
+    return ip_address
