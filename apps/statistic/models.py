7c31395874896917e052267c6d6e302ff49d4b5a
Sergey Lagovskiy <slagovskiy@gmail.com>
2013-03-20 10:20:34 +0700

    add visitor model

--- apps/statistic/models.py
+++ apps/statistic/models.py
@@ -1,3 +1,29 @@
+from datetime import datetime
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Avg, Max, Min, Count
 
-# Create your models here.
+import logging
+
+class Visitor(models.Model):
+    point = models.CharField(max_length=255, default='', verbose_name=u'Point',)
+    session_key = models.CharField(max_length=40, null=True, blank=True, db_index=True, verbose_name='Session id',)
+    ip_address = models.CharField(max_length=15, default='', null=True, blank=True, verbose_name=u'IP',)
+    user_agent = models.CharField(max_length=255, default='', null=True, blank=True, verbose_name=u'User agent',)
+    referer = models.CharField(max_length=255, default='', null=True, blank=True, verbose_name=u'Referer',)
+    user = models.ForeignKey(User, null=True, blank=True, verbose_name=u'User',)
+    date = models.DateTimeField(auto_now_add=True, verbose_name=u'Published',)
+
+    def __unicode__(self):
+        return "%s %s", (self.date, self.point)
+
+    class Meta:
+        ordering = ['date']
+
+def visitor_count(point):
+    c = 0
+    try:
+        c = Visitor.objects.filter(point=point).aggregate(Count('id'))['id__count']
+    except:
+        logging.exception('Error get count of visitors')
+    return c
