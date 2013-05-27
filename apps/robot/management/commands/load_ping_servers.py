42057c505724d8316b7a98f1f60763f0a5a06563
Sergey Lagovskiy <slagovskiy@gmail.com>
2013-05-27 16:15:49 +0700

    load ping servers

--- apps/robot/management/commands/load_ping_servers.py
+++ apps/robot/management/commands/load_ping_servers.py
@@ -1,1 +1,26 @@
-__author__ = 'lagovskiy'
+# -*- encoding: utf-8 -*-
+import logging
+
+from apps.robot.models import PingServer
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    args = 'file_name'
+    help = 'sending ping to search server'
+
+    def handle(self, *args, **options):
+        try:
+            f = open(args[0], 'r')
+            lines = f.readlines()
+            for line in lines:
+                _ping = PingServer.objects.all().filter(address=line)
+                if _ping.count()==0:
+                    ping = PingServer.objects.create(address=line)
+                    ping.save()
+                    try:
+                        logging.info('Adding server ' + line + ' - ok')
+                    except:
+                        logging.exception('Error adding server ' + line)
+        except:
+            logging.exception('Error load ping servers')
+
