25746a6b6360c31090a48c0c207fe374301bc831
Sergey Lagovskiy <slagovskiy@gmail.com>
2013-05-27 13:09:36 +0700

    ping model

---
+++ apps/robot/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from apps.blog.models import Post
+
+class PingServer(models.Model):
+    PING_PAGE = 1
+    PING_RSS = 2
+    PING_TYPE = (
+        (PING_PAGE, '1'),
+        (PING_RSS, '2'),
+    )
+    address = models.CharField(max_length=255, verbose_name=u'Address',)
+    deleted = models.BooleanField(default=False, verbose_name=u'Deleted',)
+    type = models.IntegerField(choices=PING_TYPE, default=1, verbose_name='Type',)
+
+    def __unicode__(self):
+        return self.address
+
+    class Meta:
+        ordering = ['address']
+
+class PingResult(models.Model):
+    date = models.DateTimeField(auto_now=True, auto_now_add=True)
+    pingserver = models.ForeignKey(PingServer)
+    post = models.ForeignKey(Post)
+    passed = models.BooleanField(default=False)
+    message = models.CharField(max_length=255, default='')
+
+    def __unicode__(self):
+        return '%s %s' %(self.date, str(self.passed))
