9f4c938fc2c4df1bc854e1c32199f8466edad21c
Sergey Lagovskiy <slagovskiy@gmail.com>
2013-05-27 12:44:04 +0700

    robot app

---
+++ apps/robot/settings.py
@@ -0,0 +1,14 @@
+try:
+    from apps.robot.settings_local import *
+except ImportError:
+    pass
+
+'''
+Example for settings_local.py
+
+ROBOT_EMAIL = 'noreply@server.com'
+ROBOT_LOGIN = 'noreply@server.com'
+ROBOT_PASSWORD = 'PASSWORD'
+ROBOT_SMTP = 'smtp.server.com'
+
+'''
