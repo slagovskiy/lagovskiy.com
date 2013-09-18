0a348f445a11aa44dc40089b2299fc72a4fa7889
Sergey Lagovskiy <slagovskiy@gmail.com>
2013-09-18 12:23:20 +0700

    admin model for blog

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from apps.blog.models import Category, Tag, Post, PostRevision, Comment
+from apps.blog.models import Category, Tag, Post, PostRevision, Comment, SubscribePost, CommentMessageQueue, PostImage
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('slug', 'name', 'sort')
@@ -24,7 +24,26 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ('slug', 'title')
     ordering = ('status', 'title')
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('published', 'post', 'allowed', 'deleted')
+    ordering = ('published', 'post')
+
+class SubscribePostAdmin(admin.ModelAdmin):
+    list_display = ('post', 'email', 'active')
+    ordering = ('post', 'email')
+
+class CommentMessageQueueAdmin(admin.ModelAdmin):
+    list_display = ('added', 'sended', 'active')
+    ordering = ('added', 'sended')
+
+class PostImageAdmin(admin.ModelAdmin):
+    list_display = ('post', 'description')
+    ordering = ('post', 'description')
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(SubscribePost, SubscribePostAdmin)
+admin.site.register(CommentMessageQueue, CommentMessageQueueAdmin)
+admin.site.register(PostImage, PostImageAdmin)
