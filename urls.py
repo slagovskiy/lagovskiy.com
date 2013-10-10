bb999779ea272084cfe458242ed2f397f26ef14d
Sergey Lagovskiy <slagovskiy@gmail.com>
2013-10-10 10:43:08 +0700

    redirect to robots.txt

--- urls.py
+++ urls.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+from django.views.generic.simple import direct_to_template
+
 admin.autodiscover()
 
 from settings import *
@@ -12,6 +14,7 @@ from settings import *
 urlpatterns = patterns('',
     url(r'^$', 'views.index'),
     url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/img/favicon.ico'}),
+    url(r'^robots\.txt$', direct_to_template, {'template': 'robots.txt', 'mimetype': 'text/plain'}),
 
     url(r'^blog/', include('apps.blog.urls')),
     url(r'^projects/', include('apps.projects.urls')),
