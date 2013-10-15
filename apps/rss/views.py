44cadb8050919354a7b6cb2f1720eb56f7f9beb3
Sergey Lagovskiy <slagovskiy@gmail.com>
2013-10-15 17:05:04 +0700

    rss response (not work)

--- apps/rss/views.py
+++ apps/rss/views.py
@@ -14,6 +14,8 @@ import logging
 from apps.blog.models import *
 from apps.blog.settings import *
 
+from django.utils import feedgenerator
+
 def custom_proc(request):
     return {
         'app_title': 'RSS',
@@ -24,7 +26,25 @@ def custom_proc(request):
     }
 
 def index(request):
-    pass
+    feed = feedgenerator.Rss201rev2Feed(
+        title=u"Poynter E-Media Tidbits",
+        link=u"http://www.poynter.org/column.asp?id=31",
+        description=u"A group Weblog by the sharpest minds in online media/journalism/publishing.",
+        language=u"en",
+    )
+    feed.add_item(
+        title="Hello",
+        link=u"http://www.holovaty.com/test/",
+        description="Testing."
+    )
+    t = loader.get_template('ajax.html')
+    c = RequestContext(
+        request,
+        {
+            'data': '<?xml version="1.0" encoding="utf-8"?><rss><channel></channel></rss>'#feed.writeString(encoding="utf-8"),
+            },
+        processors=[custom_proc])
+    return HttpResponse(t.render(c))
 
 def postby_tag(request, tag):
     pass
