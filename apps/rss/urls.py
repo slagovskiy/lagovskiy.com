a123346406658b70cb705bdaae3b6312b5d8f86c
Sergey Lagovskiy <slagovskiy@gmail.com>
2013-10-15 17:04:49 +0700

    rss urls

--- apps/rss/urls.py
+++ apps/rss/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^$', 'apps.rss.index'),
-    url(r'^tag/(?P<tag>[-\w]+)/$', 'apps.rss.postby_tag'),
-    url(r'^category/(?P<category>[-\w]+)/$', 'apps.rss.postby_category'),
-    url(r'^/comment/(?P<post_id>[-\w]+)/$', 'apps.rss.comment'),
+    url(r'^$', 'apps.rss.views.index'),
+    url(r'^tag/(?P<tag>[-\w]+)/$', 'apps.rss.views.postby_tag'),
+    url(r'^category/(?P<category>[-\w]+)/$', 'apps.rss.views.postby_category'),
+    url(r'^/comment/(?P<post_id>[-\w]+)/$', 'apps.rss.views.comment'),
 )
 
