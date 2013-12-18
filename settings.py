606d04b5816e2b8b095435eb53c8ef3eedb49425
Sergey Lagovskiy <slagovskiy@gmail.com>
2013-12-18 15:34:19 +0700

    debug mode moved to settings_local

--- settings.py
+++ settings.py
@@ -61,10 +61,11 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-if DEBUG:
-    STATIC_ROOT = ''
-else:
-    STATIC_ROOT = op.join(PROJECT_ROOT, 'static')
+#if DEBUG:
+#    STATIC_ROOT = ''
+#else:
+#    STATIC_ROOT = op.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
