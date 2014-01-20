931a23a43f23b37c86c891a4e852fba668a86026
Sergey Lagovskiy <slagovskiy@gmail.com>
2014-01-20 12:13:48 +0700

    refactor admin urls

--- apps/admin/urls.py
+++ apps/admin/urls.py
@@ -3,6 +3,11 @@ from django.conf.urls import patterns, include, url
 from apps.admin.views import *
 from apps.admin.views_stat import *
 from apps.admin.views_blog_category import *
+from apps.admin.views_blog_tag import *
+from apps.admin.views_blog_post import *
+from apps.admin.views_blog_revision import *
+from apps.admin.views_blog_comment import *
+from apps.admin.views_blog_image import *
 
 urlpatterns = patterns('',
     url(r'^$', 'apps.admin.views.index'),
@@ -23,39 +28,39 @@ urlpatterns = patterns('',
     url(r'^blog/category/movedown/(?P<id>[-\w]+)/', blog_category_movedown),
     url(r'^blog/category/', blog_category),
 
-    url(r'^blog/tag/getall/', 'apps.admin.views.blog_tag_getall'),
-    url(r'^blog/tag/getlist/', 'apps.admin.views.blog_tag_getlist'),
-    url(r'^blog/tag/save/', 'apps.admin.views.blog_tag_save'),
-    url(r'^blog/tag/edit/(?P<tag_id>[-\w]+)/', 'apps.admin.views.blog_tag_edit'),
-    url(r'^blog/tag/moveup/(?P<tag_id>[-\w]+)/', 'apps.admin.views.blog_tag_moveup'),
-    url(r'^blog/tag/movedown/(?P<tag_id>[-\w]+)/', 'apps.admin.views.blog_tag_movedown'),
-    url(r'^blog/tag/', 'apps.admin.views.blog_tag'),
+    url(r'^blog/tag/getall/', blog_tag_getall),
+    url(r'^blog/tag/getlist/', blog_tag_getlist),
+    url(r'^blog/tag/save/', blog_tag_save),
+    url(r'^blog/tag/edit/(?P<tag_id>[-\w]+)/', blog_tag_edit),
+    url(r'^blog/tag/moveup/(?P<tag_id>[-\w]+)/', blog_tag_moveup),
+    url(r'^blog/tag/movedown/(?P<tag_id>[-\w]+)/', blog_tag_movedown),
+    url(r'^blog/tag/', blog_tag),
 
-    url(r'^blog/post/getall/', 'apps.admin.views.blog_post_getall'),
-    url(r'^blog/post/save/', 'apps.admin.views.blog_post_save'),
-    url(r'^blog/post/edit/(?P<id>[-\w]+)/', 'apps.admin.views.blog_post_edit'),
-    url(r'^blog/post/', 'apps.admin.views.blog_post'),
+    url(r'^blog/post/getall/', blog_post_getall),
+    url(r'^blog/post/save/', blog_post_save),
+    url(r'^blog/post/edit/(?P<post_id>[-\w]+)/', blog_post_edit),
+    url(r'^blog/post/', blog_post),
 
-    url(r'^blog/postimage/save/', 'apps.admin.views.blog_postimage_save'),
-    url(r'^blog/postimage/delete/(?P<id>[-\w]+)', 'apps.admin.views.blog_postimage_delete'),
-    url(r'^blog/postimage/insert/(?P<id>[-\w]+)', 'apps.admin.views.blog_postimage_insert'),
-    url(r'^blog/postimage/edit/(?P<post_id>[-\w]+)/(?P<id>[-\w]+)', 'apps.admin.views.blog_postimage_edit'),
-    url(r'^blog/postimage/(?P<post_id>[-\w]+)/', 'apps.admin.views.blog_postimage_list'),
+    url(r'^blog/postimage/save/', blog_postimage_save),
+    url(r'^blog/postimage/delete/(?P<image_id>[-\w]+)', blog_postimage_delete),
+    url(r'^blog/postimage/insert/(?P<image_id>[-\w]+)', blog_postimage_insert),
+    url(r'^blog/postimage/edit/(?P<post_id>[-\w]+)/(?P<image_id>[-\w]+)', blog_postimage_edit),
+    url(r'^blog/postimage/(?P<post_id>[-\w]+)/', blog_postimage_list),
 
-    url(r'^blog/comment/save/', 'apps.admin.views.blog_comment_save'),
-    url(r'^blog/comment/restore/(?P<id>[-\w]+)/', 'apps.admin.views.blog_comment_restore'),
-    url(r'^blog/comment/getlist/(?P<id>[-\w]+)/', 'apps.admin.views.blog_comment_getlist'),
-    url(r'^blog/comment/edit/(?P<id>[-\w]+)/', 'apps.admin.views.blog_comment_edit'),
-    url(r'^blog/comment/delete/(?P<id>[-\w]+)/', 'apps.admin.views.blog_comment_delete'),
-    url(r'^blog/comment/allow/(?P<id>[-\w]+)/', 'apps.admin.views.blog_comment_allow'),
+    url(r'^blog/comment/save/', blog_comment_save),
+    url(r'^blog/comment/restore/(?P<comment_id>[-\w]+)/', blog_comment_restore),
+    url(r'^blog/comment/getlist/(?P<post_id>[-\w]+)/', blog_comment_getlist),
+    url(r'^blog/comment/edit/(?P<comment_id>[-\w]+)/', blog_comment_edit),
+    url(r'^blog/comment/delete/(?P<comment_id>[-\w]+)/', blog_comment_delete),
+    url(r'^blog/comment/allow/(?P<comment_id>[-\w]+)/', blog_comment_allow),
 
-    url(r'^blog/revision/getlist/(?P<post_id>[-\w]+)/', 'apps.admin.views.blog_revision_getlist'),
-    url(r'^blog/revision/getcontent/(?P<id>[-\w]+)/', 'apps.admin.views.blog_revision_getcontent'),
-    url(r'^blog/revision/getteaser/(?P<id>[-\w]+)/', 'apps.admin.views.blog_revision_getteaser'),
-    url(r'^blog/revision/create/(?P<post_id>[-\w]+)/', 'apps.admin.views.blog_revision_create'),
-    url(r'^blog/revision/save/(?P<post_id>[-\w]+)/', 'apps.admin.views.blog_revision_save'),
-    url(r'^blog/revision/fix/(?P<id>[-\w]+)/', 'apps.admin.views.blog_revision_fix'),
-    url(r'^blog/revision/preview/(?P<id>[-\w]+)/', 'apps.admin.views.blog_revision_preview'),
+    url(r'^blog/revision/getlist/(?P<post_id>[-\w]+)/', blog_revision_getlist),
+    url(r'^blog/revision/getcontent/(?P<revision_id>[-\w]+)/', blog_revision_getcontent),
+    url(r'^blog/revision/getteaser/(?P<revision_id>[-\w]+)/', blog_revision_getteaser),
+    url(r'^blog/revision/create/(?P<post_id>[-\w]+)/', blog_revision_create),
+    url(r'^blog/revision/save/(?P<post_id>[-\w]+)/', blog_revision_save),
+    url(r'^blog/revision/fix/(?P<revision_id>[-\w]+)/', blog_revision_fix),
+    url(r'^blog/revision/preview/(?P<revision_id>[-\w]+)/', blog_revision_preview),
 
     url(r'^banner/getall/', 'apps.admin.views.banner_getall'),
     url(r'^banner/save/', 'apps.admin.views.banner_save'),
