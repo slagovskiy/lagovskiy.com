7f50498c4d9e0b024fc943110829269e87620f9d
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-04-21 11:56:26 +0600

    blog admin

--- odyssey/apps/blog/admin.py
+++ odyssey/apps/blog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Category
+from .models import Tag
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ['sort', 'slug', 'name', 'deleted']
+    ordering = ['sort', 'name']
+    list_display = ['slug', 'name', 'sort', 'deleted']
+    list_filter = ['slug']
+    search_fields = ['slug']
+
+
+class TagAdmin(admin.ModelAdmin):
+    fields = ['sort', 'slug', 'name', 'deleted']
+    ordering = ['sort', 'name']
+    list_display = ['slug', 'name', 'sort', 'deleted']
+    list_filter = ['slug']
+    search_fields = ['slug']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
