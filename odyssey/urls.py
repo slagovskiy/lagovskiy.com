de97b628aa26f91dc585323d7989ec1d1e575614
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-04-21 12:03:47 +0600

    index view and urls

--- odyssey/urls.py
+++ odyssey/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'odyssey.views.home', name='home'),
+    url(r'^$', 'odyssey.views.index', name='index'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
