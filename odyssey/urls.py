d2a884f8b2384f88ce0e09201427fc47d8b87056
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-04-21 17:10:13 +0600

    images

--- odyssey/urls.py
+++ odyssey/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from odyssey.settings import base
 
 urlpatterns = [
     url(r'^$', 'odyssey.views.index', name='index'),
@@ -7,3 +9,5 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+# urlpatterns += static(base.STATIC_URL, document_root=base.STATIC_ROOT)
