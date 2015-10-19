24cbcd4fa9c66486c6eed188e877e8d8679dbc29
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-10-19 15:08:34 +0600

    clear

--- odyssey/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def index(request):
-    ids = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    context = {'ids': ids}
-    return render(request, 'index.html', context)
