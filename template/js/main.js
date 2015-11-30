e5298a78658ffcfd5606baa8fa9f9c52c24f9a4a
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-11-30 12:26:59 +0600

    jBox notice function

--- template/js/main.js
+++ template/js/main.js
@@ -1,1 +1,11 @@
 /* lagovskiy.com */
+
+function notice(color, message){
+    new jBox('Notice', {
+        content: message,
+        color: color,
+        animation:{open:'slide:bottom',close:'slide:left'},
+        attributes:{x:'left',y:'bottom'},
+        theme:'NoticeBorder',
+    });
+}
