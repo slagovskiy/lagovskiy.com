c2b7b3f2d463fc91765d9f5cccb7e363e7ca6461
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-12-02 17:01:01 +0600

    set AUTH_USER_MODEL to userext.user

--- src/odyssey/settings.py
+++ src/odyssey/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.userext',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -119,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+LOGIN_URL = r"/login/"
+
+AUTH_USER_MODEL = 'userext.User'
