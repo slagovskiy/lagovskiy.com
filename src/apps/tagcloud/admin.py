7933c5e5f80edac4bc37206de6083527775c931a
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-12-08 17:37:33 +0600

    tag admin

--- src/apps/tagcloud/admin.py
+++ src/apps/tagcloud/admin.py
@@ -3,7 +3,8 @@ from .models import Tag
 
 
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug', 'deleted')
-
+    list_display = ('name', 'slug', 'block', 'deleted')
+    list_filter = ['block']
+    
 
 admin.site.register(Tag, TagAdmin)
