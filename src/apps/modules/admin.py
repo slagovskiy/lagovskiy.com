49dafa9d34f5220b5f12af31952ffddc57d7fb18
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-12-08 16:31:11 +0600

    module admin

--- src/apps/modules/admin.py
+++ src/apps/modules/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Module
 
-# Register your models here.
+
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'deleted')
+
+
+admin.site.register(Module, ModuleAdmin)
