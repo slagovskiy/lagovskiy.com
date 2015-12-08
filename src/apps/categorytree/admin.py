fa12656f884b75afb6fd08f00c1dd8c5672b4670
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-12-08 17:35:43 +0600

    tag admin

--- src/apps/categorytree/admin.py
+++ src/apps/categorytree/admin.py
@@ -3,8 +3,8 @@ from .models import Category
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'parent', 'deleted')
-    list_filter = ('name', 'parent')
+    list_display = ('name', 'parent', 'block', 'deleted')
+    list_filter = ('name', 'parent', 'block')
 
 
 admin.site.register(Category, CategoryAdmin)
