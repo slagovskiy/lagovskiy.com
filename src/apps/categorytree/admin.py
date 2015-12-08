8eec73854cc7188067f0e1d13ecb7bad4c495844
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-12-08 14:45:09 +0600

    Category admin

--- src/apps/categorytree/admin.py
+++ src/apps/categorytree/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+from .models import Category
 
-# Register your models here.
+
+class CategoryMPTTModelAdmin(MPTTModelAdmin):
+    mptt_level_indent = 20
+
+
+admin.site.register(Category, CategoryMPTTModelAdmin)
