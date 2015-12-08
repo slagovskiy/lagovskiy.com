7d8bb6ef42de23f52aa27001e3025c59d7e31073
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-12-08 16:31:02 +0600

    module model

--- src/apps/modules/models.py
+++ src/apps/modules/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Module(models.Model):
+    slug = models.SlugField(
+        unique=True
+    )
+    name = models.CharField(
+        max_length=255,
+        default=''
+    )
+    deleted = models.BooleanField(
+        default=False
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Module'
+        verbose_name_plural = 'Modules'
