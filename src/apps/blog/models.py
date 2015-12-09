d1be44c587033ae12f62709d11d43da0144fcb8a
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-12-09 10:04:33 +0600

    category model in blog app

--- src/apps/blog/models.py
+++ src/apps/blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    slug = models.SlugField(
+        unique=True
+    )
+    name = models.CharField(
+        max_length=255,
+        default=''
+    )
+    deleted = models.BooleanField(
+        default=False
+    )
+    parent = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        related_name='children',
+        db_index=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
