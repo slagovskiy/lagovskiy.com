047b882aef13e287b030d348bf2596102acb5adc
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-12-09 10:05:09 +0600

    tag model in blog app

--- src/apps/blog/models.py
+++ src/apps/blog/models.py
@@ -27,3 +27,24 @@ class Category(models.Model):
         ordering = ['name']
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
+
+
+class Tag(models.Model):
+    slug = models.SlugField(
+        unique=True
+    )
+    name = models.CharField(
+        max_length=255,
+        default=''
+    )
+    deleted = models.BooleanField(
+        default=False
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
