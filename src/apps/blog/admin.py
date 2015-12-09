240dd9a27f103445ab66b7a85aef906bd84ab045
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-12-09 10:07:13 +0600

    category admin in blog app

--- src/apps/blog/admin.py
+++ src/apps/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category, Tag
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent', 'deleted')
+    list_filter = ('block', 'name', 'parent')
+
+
+admin.site.register(Category, CategoryAdmin)
