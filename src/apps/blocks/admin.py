86b37dd89db0fa15aaf87151f04f12bb3703ed43
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-12-09 10:12:47 +0600

    deleted blocks app

--- src/apps/blocks/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Block
-
-
-class BlockAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug', 'deleted')
-
-
-admin.site.register(Block, BlockAdmin)
