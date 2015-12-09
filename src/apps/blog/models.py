9dbd4c646ca69a08b01614e103f1b0895db6e45d
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-12-09 10:46:58 +0600

    post model

--- src/apps/blog/models.py
+++ src/apps/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from apps.userext.models import User
 
 
 class Category(models.Model):
@@ -48,3 +49,62 @@ class Tag(models.Model):
         ordering = ['name']
         verbose_name = 'Tag'
         verbose_name_plural = 'Tags'
+
+
+class Post(models.Model):
+    DRAFT_STATUS = 1
+    PUBLISHED_STATUS = 2
+    HIDDEN_STATUS = 3
+
+    STATUS_CHOICES = (
+        (DRAFT_STATUS, 'Draft'),
+        (PUBLISHED_STATUS, 'Public'),
+        (HIDDEN_STATUS, 'Hidden'),
+    )
+
+    slug = models.SlugField(
+        max_length=255
+    )
+    title = models.TextField(
+        max_length=255
+    )
+    author = models.ForeignKey(
+        User,
+        blank=True
+    )
+    description = models.CharField(
+        max_length=512,
+        default=''
+    )
+    keywords = models.CharField(
+        max_length=512,
+        default=''
+    )
+    status = models.IntegerField(
+        choices=STATUS_CHOICES,
+        default=1
+    )
+    sticked = models.BooleanField(
+        default=False
+    )
+    comments_enabled = models.BooleanField(
+        default=True
+    )
+    comments_moderated = models.BooleanField(
+        default=True
+    )
+    do_ping = models.BooleanField(
+        default=False
+    )
+    published = models.DateTimeField(
+        blank=True,
+        null=True
+    )
+    categories = models.ManyToManyField(
+        Category,
+        blank=True
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        blank=True
+    )
