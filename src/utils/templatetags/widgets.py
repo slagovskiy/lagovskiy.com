bddb30d8be557b7cb127992419973393ca7076d3
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-12-10 10:21:34 +0600

    tag widget

--- src/utils/templatetags/widgets.py
+++ src/utils/templatetags/widgets.py
@@ -1,9 +1,10 @@
 from django import template
-
+from apps.blog.models import Tag
 
 register = template.Library()
 
 
 @register.inclusion_tag('widgets/tags.html')
 def widget_tags():
-    return {'tags': '123'}
+    tags = Tag.objects.all().filter(deleted=False)
+    return {'tags': tags}
