325cc88bf568dee54208eff82dc3afb90656a9d2
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-12-10 14:50:15 +0600

    tag url function

--- src/apps/blog/models.py
+++ src/apps/blog/models.py
@@ -48,6 +48,9 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return '/blog/tag/%s' % self.slug
+
     def post_count(self):
         return self.post_set.all().filter(status=2).count()
 
