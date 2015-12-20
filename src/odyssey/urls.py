22af4d7fcce3d3902d8e8cd1bf50408e0c393349
Sergey Lagovskiy <Sergey Lagovskiy>
2015-12-20 23:05:19 +0600

    include admin urls

--- src/odyssey/urls.py
+++ src/odyssey/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 from .views import index
 
+
 urlpatterns = [
     url(r'^$', index, name='home'),
+    url(r'^oadmin/', include('apps.oadmin.urls')),
     url(r'^admin/', admin.site.urls),
 ]
