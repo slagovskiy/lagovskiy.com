bde43c17bf966b91f62cda3163361b4d2acc3f9a
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-12-21 17:22:12 +0600

    edit tag

--- src/apps/oadmin/urls.py
+++ src/apps/oadmin/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from .views import index
-from .views import tag, tag_all
+from .views import tag, tag_all, tag_edit
 
 
 urlpatterns = [
     url(r'^$', index, name='oadmin_dashboard'),
     url(r'^tag/$', tag, name='oadmin_tag'),
     url(r'^tag/all/$', tag_all, name='oadmin_tag_all'),
+    url(r'^tag/(?P<id>[-\w]+)/$', tag_edit, name='oadmin_tag_edit'),
 ]
--- src/apps/oadmin/views.py
+++ src/apps/oadmin/views.py
@@ -19,3 +19,8 @@ def tag(request):
 def tag_all(request):
     data = serializers.serialize('json', Tag.objects.all())
     return JsonResponse(data, safe=False)
+
+
+def tag_edit(request, id):
+    data = serializers.serialize('json', Tag.objects.all().filter(id=id))
+    return JsonResponse(data, safe=False)
