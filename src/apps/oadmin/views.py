712eba5fb3977dcc1ea0a7c9b0f90614eb44c4ed
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-12-21 13:58:44 +0600

    tag view

--- src/apps/oadmin/views.py
+++ src/apps/oadmin/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.core import serializers
+from django.http import JsonResponse
+from apps.blog.models import Tag
 
 
 def index(request):
     content = {
     }
     return render(request, 'oadmin/index.html', content)
+
+
+def tag(request):
+    content = {
+    }
+    return render(request, 'oadmin/tag.html', content)
