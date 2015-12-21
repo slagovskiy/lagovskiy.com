5fe8dd15ea21e0fe133d3ee5ef2b8eff66607e42
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-12-21 13:58:15 +0600

    tag

--- src/apps/oadmin/urls.py
+++ src/apps/oadmin/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from .views import index
+from .views import tag, tag_all
+
 
 urlpatterns = [
-    url(r'^$', index, name='home'),
+    url(r'^$', index, name='oadmin_dashboard'),
+    url(r'^tag/$', tag, name='oadmin_tag'),
 ]
