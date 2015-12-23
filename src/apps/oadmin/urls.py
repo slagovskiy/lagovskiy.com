8b5644aa89d8aedc8d359bf5dd949a67816a8343
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-12-23 10:56:52 +0600

    add new

--- src/apps/oadmin/urls.py
+++ src/apps/oadmin/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 from .views import index
-from .views import tag, tag_all, tag_edit, tag_save
+from .views import tag, tag_all, tag_edit, tag_new, tag_save
 
 
 urlpatterns = [
     url(r'^$', index, name='oadmin_dashboard'),
     url(r'^blog/tag/$', tag, name='oadmin_tag'),
     url(r'^blog/tag/all/$', tag_all, name='oadmin_tag_all'),
+    url(r'^blog/tag/new/$', tag_new, name='oadmin_tag_new'),
     url(r'^blog/tag/save/$', tag_save, name='oadmin_tag_save'),
     url(r'^blog/tag/(?P<id>[-\w]+)/$', tag_edit, name='oadmin_tag_edit'),
 ]
