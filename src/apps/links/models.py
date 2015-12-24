3e69c3f876542d828b6246c5abeba0018840e675
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-12-24 16:31:10 +0600

    MyLinks model

--- src/apps/links/models.py
+++ src/apps/links/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class MyLinks(models.Model):
+    name = models.CharField(
+        max_length=255,
+        default=''
+    )
+    link = models.CharField(
+        max_length=255,
+        default=''
+    )
+    order = models.SmallIntegerField(
+        default=0
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['order', 'name']
