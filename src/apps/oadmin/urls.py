e39bcc4504e9404cfe68c1171968f3a0138b9e06
Sergey Lagovskiy <slagovskiy@gmail.com>
2015-12-24 17:30:36 +0600

    mylinks urls

--- src/apps/oadmin/urls.py
+++ src/apps/oadmin/urls.py
@@ -2,10 +2,16 @@ from django.conf.urls import url
 from .views import index
 from .views import tag, tag_all, tag_edit, tag_save
 from .views import category, category_all, category_edit, category_save
+from .views import mylinks, mylinks_all, mylinks_edit, mylinks_save
 
 
 urlpatterns = [
     url(r'^$', index, name='oadmin_dashboard'),
+
+    url(r'^links/mylinks/$', mylinks, name='oadmin_mylinks'),
+    url(r'^links/mylinks/all/$', mylinks_all, name='oadmin_mylinks_all'),
+    url(r'^links/mylinks/save/$', mylinks_save, name='oadmin_mylinks_save'),
+    url(r'^links/mylinks/(?P<id>[-\w]+)/$', mylinks_edit, name='oadmin_mylinks_edit'),
 
     url(r'^blog/tag/$', tag, name='oadmin_tag'),
     url(r'^blog/tag/all/$', tag_all, name='oadmin_tag_all'),
