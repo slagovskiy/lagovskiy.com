f25752a5db10fe8529bb6d05fd511c3d7a741376
Sergey Lagovskiy <slagovskiy@gmail.com>
2016-02-02 16:38:24 +0600

    main js

---
+++ src/project/static/js/main.js
@@ -0,0 +1,39 @@
+/* lagovskiy.com */
+
+function notice(color, message){
+    new jBox('Notice', {
+        content: message,
+        color: color,
+        animation:{open:'slide:bottom',close:'slide:left'},
+        attributes:{x:'left',y:'bottom'},
+        theme:'NoticeBorder',
+    });
+}
+
+function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+};
+
+
+$(document).ready(function() {
+    $('#head-menu-link').click(function(){
+        $('#head-menu-line').slideToggle();
+    });
+    /*
+     //http://api.jquery.com/jquery.ajax/
+     $("form").submit(function() {
+         $.ajax({
+            type: "GET",
+            url: "\save\",
+            data: $("form").serialize()
+        }).done(function() {
+            alert("sended");
+            setTimeout(function() {
+                $.fancybox.close();
+            }, 1000);
+        });
+        return false;
+     });
+     */
+});
