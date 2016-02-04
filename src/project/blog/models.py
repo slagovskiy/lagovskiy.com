92a1638b3fe1e1696be0e6853b21f0e8a0c856c0
Sergey Lagovskiy <slagovskiy@gmail.com>
2016-02-04 12:03:21 +0600

    category model

--- src/project/blog/models.py
+++ src/project/blog/models.py
@@ -7,7 +7,7 @@ from .config import SIZE_NAME, SIZE_UUID
 class Tag(db.Model):
     __tablename__ = 'blog_tag'
     id = db.Column('tag_id', db.Integer, primary_key=True)
-    uuid = db.Column('uuid', db.String(SIZE_UUID))
+    uuid = db.Column('uuid', db.String(SIZE_UUID), unique=True)
     slug = db.Column('slug', db.String(SIZE_NAME), unique=True, index=True)
     tagname = db.Column('tagname', db.String(SIZE_NAME), index=True)
     added = db.Column('added', db.DateTime)
@@ -21,3 +21,22 @@ class Tag(db.Model):
 
     def __repr__(self):
         return '<Tag %s [$s]>' % (self.tagname, self.slug)
+
+
+class Category(db.Model):
+    __tablename__ = 'blog_category'
+    id = db.Column('category_id', db.Integer, primary_key=True)
+    uuid = db.Column('uuid', db.String(SIZE_UUID), unique=True)
+    slug = db.Column('slug', db.String(SIZE_NAME), unique=True, index=True)
+    categoryname = db.Column('categoryname', db.String(SIZE_NAME), index=True)
+    added = db.Column('added', db.DateTime)
+    deleted = db.Column('deleted', db.Boolean, default=False)
+
+    def __init__(self, slug, categoryname):
+        self.uuid = str(uuid4())
+        self.slug = slug
+        self.categoryname = categoryname
+        self.added = datetime.utcnow()
+
+    def __repr__(self):
+        return '<Category %s [$s]>' % (self.categoryname, self.slug)
