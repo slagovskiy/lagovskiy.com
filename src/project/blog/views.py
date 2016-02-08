a7648d4a2ac5e07fb0f401658bc1fb2af0644742
Sergey Lagovskiy <slagovskiy@gmail.com>
2016-02-08 17:16:41 +0600

    posts view

--- src/project/blog/views.py
+++ src/project/blog/views.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, g
+from flask import Blueprint, render_template, g, redirect, url_for
 from project import app
-from .models import Tag, Category
+from sqlalchemy import func
+from .models import Tag, Category, Post
+from .config import POST_STATUS_PUBLISHED
 
 mod_blog = Blueprint(
     'blog',
@@ -18,14 +20,38 @@ def before_request():
 
 @mod_blog.route('/', methods=['GET'])
 def index():
-    return render_template('posts.html')
+    posts = Post.query.filter_by(status=POST_STATUS_PUBLISHED)
+    return render_template(
+        'posts.html',
+        posts=posts
+    )
 
 
 @mod_blog.route('/tag/<slug>', methods=['GET'])
 def posts_by_tag(slug=None):
-    return render_template('posts.html')
+    t = Tag.query.filter(func.lower(slug) == func.lower(Tag.slug)).first()
+    posts = None
+    if t:
+        posts = t.posts.filter_by(status=POST_STATUS_PUBLISHED).order_by('-published').all()
+        return render_template(
+            'posts.html',
+            posts=posts,
+            active_tag=t
+        )
+    else:
+        return redirect(url_for('blog.index'))
 
 
 @mod_blog.route('/category/<slug>', methods=['GET'])
 def posts_by_category(slug=None):
-    return render_template('posts.html')
+    c = Category.query.filter(func.lower(slug) == func.lower(Category.slug)).first()
+    posts = None
+    if c:
+        posts = c.posts.filter_by(status=POST_STATUS_PUBLISHED).order_by('-published').all()
+        return render_template(
+            'posts.html',
+            posts=posts,
+            active_category=c
+        )
+    else:
+        return redirect(url_for('blog.index'))
