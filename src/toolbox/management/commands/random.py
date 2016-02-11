2fbe5f3d0bf8f66915d260a1df5b9d1d6076eff5
Sergey Lagovskiy <slagovskiy@gmail.com>
2016-02-11 16:22:37 +0600

    random generator fake data

---
+++ src/toolbox/management/commands/random.py
@@ -0,0 +1,33 @@
+from random import randint
+from apps.blog.models import Tag
+from faker import Factory
+from django.core.management.base import BaseCommand
+
+fake_ru = Factory.create('ru-RU')
+fake_en = Factory.create('en-US')
+fake = None
+
+
+def blog_tag(count):
+    Tag.objects.all().delete()
+    for _ in range(0, count):
+        if randint(0, 1):
+            fake = fake_ru
+        else:
+            fake = fake_en
+        slug = fake.slug()
+        name = fake.word()
+        if not Tag.exist(slug):
+            t = Tag.objects.create(
+                slug=slug,
+                name=name
+            )
+            t.save()
+            print(t)
+
+
+class Command(BaseCommand):
+    help = 'generate random data'
+
+    def handle(self, **options):
+        blog_tag(50)
