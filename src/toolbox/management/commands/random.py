480fcea0fd54ac005f45d4127815d100c5b3b2f0
Sergey Lagovskiy <slagovskiy@gmail.com>
2016-02-13 00:51:22 +0600

    generate category

--- src/toolbox/management/commands/random.py
+++ src/toolbox/management/commands/random.py
@@ -1,5 +1,5 @@
 from random import randint
-from apps.blog.models import Tag
+from apps.blog.models import Tag, Category
 from faker import Factory
 from django.core.management.base import BaseCommand
 
@@ -8,8 +8,11 @@ fake_en = Factory.create('en-US')
 fake = None
 
 
-def blog_tag(count):
+def clean():
     Tag.objects.all().delete()
+    Category.objects.all().delete()
+
+def blog_tag(count):
     for _ in range(0, count):
         if randint(0, 1):
             fake = fake_ru
@@ -26,8 +29,27 @@ def blog_tag(count):
             print(t)
 
 
+def blog_category(count):
+    for _ in range(0, count):
+        if randint(0, 1):
+            fake = fake_ru
+        else:
+            fake = fake_en
+        slug = fake.slug()
+        name = fake.word()
+        if not Category.exist(slug):
+            c = Category.objects.create(
+                slug=slug,
+                name=name
+            )
+            c.save()
+            print(c)
+
+
 class Command(BaseCommand):
     help = 'generate random data'
 
     def handle(self, **options):
+        clean()
+        blog_category(10)
         blog_tag(50)
