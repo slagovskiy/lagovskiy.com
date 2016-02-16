01ed40285a31d6a8913e7ece39202b11bc9c15d4
Sergey Lagovskiy <slagovskiy@gmail.com>
2016-02-16 16:12:35 +0600

    generate mylinks

--- src/toolbox/management/commands/random.py
+++ src/toolbox/management/commands/random.py
@@ -1,5 +1,6 @@
 from random import randint
 from apps.blog.models import Tag, Category
+from apps.links.models import MyLink
 from faker import Factory
 from django.core.management.base import BaseCommand
 
@@ -9,8 +10,65 @@ fake = None
 
 
 def clean():
+    MyLink.objects.all().delete()
     Tag.objects.all().delete()
     Category.objects.all().delete()
+
+
+def links_mylink():
+    slug = 'facebook'
+    name = '<i class="fa fa-facebook-official"></i> i on facebook'
+    link = 'https://www.facebook.com/sergey.lagovskiy'
+    ml = MyLink.objects.create(
+        slug=slug,
+        name=name,
+        link=link,
+        order=10
+    )
+    ml.save()
+    slug = 'flickr'
+    name = '<i class="fa fa-flickr"></i>  my photos on flickr'
+    link = 'https://www.flickr.com/photos/slagovskiy/'
+    ml = MyLink.objects.create(
+        slug=slug,
+        name=name,
+        link=link,
+        order=20
+    )
+    ml.save()
+    slug = 'github'
+    name = '<i class="fa fa-github-square"></i> my code on github'
+    link = 'https://github.com/slagovskiy'
+    ml = MyLink.objects.create(
+        slug=slug,
+        name=name,
+        link=link,
+        order=30
+    )
+    ml.save()
+    slug = 'email'
+    name = '<i class="fa fa-envelope-square"></i> write me email'
+    link = 'mailto:slagovskiy@gmail.com'
+    blank = False
+    ml = MyLink.objects.create(
+        slug=slug,
+        name=name,
+        link=link,
+        order=40,
+        blank=blank
+    )
+    ml.save()
+    slug = 'hh'
+    name = '<i class="fa fa-user"></i> headhunt me'
+    link = 'http://novosibirsk.hh.ru/resume/4f27274aff01958a560039ed1f7a44564d3256'
+    ml = MyLink.objects.create(
+        slug=slug,
+        name=name,
+        link=link,
+        order=50
+    )
+    ml.save()
+
 
 def blog_tag(count):
     for _ in range(0, count):
