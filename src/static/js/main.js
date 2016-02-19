748f57ddc1f03a1121217e596985013f16a10dda
Sergey Lagovskiy <slagovskiy@gmail.com>
2016-02-19 15:49:50 +0600

    datetime jsrender filter

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -21,18 +21,25 @@ $(document).ready(function() {
         $('#head-menu-line').slideToggle();
     });
 
+    $('.dateMask').mask('0000/00/00');
+    $('.datetimeMask').mask('0000/00/00 00:00:00');
+
     $.views.converters({
         datetime: function(value) {
-            var date = '/';
-            var time = ':';
-            if (this.tagCtx.props.date)
-                date = this.tagCtx.props.date;
-            if (this.tagCtx.props.time)
-                time = this.tagCtx.props.time;
-            var dt = value.split('T');
-            var d = dt[0].split('-');
-            var t = dt[1].replace('T', '').replace('Z', '').split(':');
-            return d[0] + date + d[1] + date + d[2] + ' ' + t[0] + time + t[1] + time + t[2].split('.')[0];
+            if ((value == '') || (value == null))
+                return value;
+            else {
+                var date = '/';
+                var time = ':';
+                if (this.tagCtx.props.date)
+                    date = this.tagCtx.props.date;
+                if (this.tagCtx.props.time)
+                    time = this.tagCtx.props.time;
+                var dt = value.split('T');
+                var d = dt[0].split('-');
+                var t = dt[1].replace('T', '').replace('Z', '').split(':');
+                return d[0] + date + d[1] + date + d[2] + ' ' + t[0] + time + t[1] + time + t[2].split('.')[0];
+            }
         }
     });
 
