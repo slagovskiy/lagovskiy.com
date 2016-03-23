26a8c8d37d6dd59d0319f1bb41fc5a9af4211898
Sergey Lagovskiy <slagovskiy@gmail.com>
2016-03-23 16:45:01 +0600

    post_view, blog_view

--- src/apps/blog/views.py
+++ src/apps/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Post, Category, Tag
 
-# Create your views here.
+
+def blog_view(request):
+    posts = Post.objects.all().filter(status=Post.PUBLISHED_STATUS)
+    content = {
+        'posts': posts
+    }
+    return render(request, 'index.html', content)
+
+def blog_post_view(request, slug=''):
+    post = Post.objects.filter(slug=slug, status=Post.PUBLISHED_STATUS).first()
+    content = {
+        'post': post
+    }
+    return render(request, 'blog/post_view.html', content)
