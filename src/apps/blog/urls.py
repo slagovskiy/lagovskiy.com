4cc2ecb494e0547609889ef26a11c92dd58bae7a
Sergey Lagovskiy <slagovskiy@gmail.com>
2016-03-30 11:24:17 +0600

    view by tag and category

--- src/apps/blog/urls.py
+++ src/apps/blog/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import blog_view, blog_post_view
+from .views import blog_view, blog_post_view, blog_post_by_tag, blog_post_by_category
 
 urlpatterns = [
     url(r'^$', blog_view, name='blog'),
-
+    url(r'^tag/(?P<slug>[-\w]+)/$', blog_post_by_tag, name='blog_post_by_tag'),
+    url(r'^category/(?P<slug>[-\w]+)/$', blog_post_by_category, name='blog_post_by_category'),
     url(r'^view/(?P<slug>[-\w]+)/$', blog_post_view, name='blog_post'),
 ]
