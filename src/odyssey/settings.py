f31a6cbd1459831697040ce617c6fde6102211d5
Sergey Lagovskiy <slagovskiy@gmail.com>
2016-04-03 00:20:18 +0600

    site map

--- src/odyssey/settings.py
+++ src/odyssey/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
 
     # 3rd party middleware
     'django_markdown',
--- src/odyssey/urls.py
+++ src/odyssey/urls.py
@@ -1,10 +1,19 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from .views import index
+from toolbox.sitemap import BlogSitemap
+
+sitemaps = {
+    'pages': BlogSitemap()
+}
 
 
 urlpatterns = [
     url(r'^$', index, name='home'),
+
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}),
+
     url(r'^blog/', include('apps.blog.urls')),
     url(r'^media/', include('apps.media.urls')),
     url(r'^admin/', include('apps.admin.urls')),
