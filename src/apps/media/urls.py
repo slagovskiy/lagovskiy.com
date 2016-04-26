d5dc1c72cb97ce82f3da953f35c975a6efc0a013
Sergey Lagovskiy <slagovskiy@gmail.com>
2016-04-26 12:13:03 +0600

    url for full path

--- src/apps/media/urls.py
+++ src/apps/media/urls.py
@@ -4,5 +4,7 @@ from .views import media_file
 
 urlpatterns = [
     url(r'^file/(?P<key>[-\w]+)/$', media_file, name='media_file'),
+    url(r'^files/(?P<y>[-\w]+)/(?P<m>[-\w]+)/(?P<d>[-\w]+)/(?P<key>[-\w]+)/$', media_file, name='media_file'),
     url(r'^file/(?P<key>[-\w]+)/(?P<filename>[-\w\.]+)$', media_file, name='media_file'),
+    url(r'^files/(?P<y>[-\w]+)/(?P<m>[-\w]+)/(?P<d>[-\w]+)/(?P<key>[-\w]+)/(?P<filename>[-\w\.]+)$', media_file, name='media_file'),
 ]
