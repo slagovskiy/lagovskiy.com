973c15876bb25b396c48264004194cfdcc7c06cb
Sergey Lagovskiy <slagovskiy@gmail.com>
2016-04-26 13:08:49 +0600

    mylinks admin forms

--- src/apps/links/admin.py
+++ src/apps/links/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import MyLink
+from .forms import MyLinkAdminForm
 
 
 class MyLinksAdmin(admin.ModelAdmin):
-    list_display = ('name', 'link', 'order')
+    list_display = ('name', 'link', 'order', 'deleted')
     ordering = ('order', 'name')
+    form = MyLinkAdminForm
 
 admin.site.register(MyLink, MyLinksAdmin)
