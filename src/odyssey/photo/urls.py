9a928b2483c03386e08b6e8f360707c76964ac2d
Sergey Lagovskiy <slagovskiy@gmail.com>
2017-04-10 13:10:54 +0700

    photo view

--- src/odyssey/photo/urls.py
+++ src/odyssey/photo/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from .views import photo_view, photo_by_album, photo_by_tag
+from .views import photo_view, photo_by_album, photo_by_tag, photo_item_view
 
 urlpatterns = [
     url(r'^$', photo_view, name='photo'),
     url(r'^tag/(?P<slug>[-\w]+)/$', photo_by_tag, name='photo_by_tag'),
     url(r'^album/(?P<slug>[-\w]+)/$', photo_by_album, name='photo_by_album'),
-    #url(r'^view/(?P<slug>[-\w]+)/$', photo_item_view, name='photo_item'),
+    url(r'^view/(?P<slug>[-\w]+)/$', photo_item_view, name='photo_item_view'),
 ]
--- src/odyssey/photo/views.py
+++ src/odyssey/photo/views.py
@@ -8,7 +8,7 @@ from .models import Album, Tag, Photo
 
 def photo_view(request):
     page = int(request.GET.get('page', 1))
-    photos = Photo.objects.all().filter(status=Photo.PUBLISHED_STATUS).order_by('-published')
+    photos = Photo.objects.all().filter(status=Photo.PUBLISHED_STATUS).order_by('published')
     paginator = Paginator(photos, PAGE_SIZE)
     if page <= 0:
         page = 1
@@ -29,3 +29,11 @@ def photo_by_album(request):
 def photo_by_tag(request):
     content = []
     return  render(request, 'photo/index.html', content)
+
+
+def photo_item_view(request, slug=''):
+    photo = Photo.objects.all().filter(slug=slug, status=Photo.PUBLISHED_STATUS).first()
+    content = {
+        'photo': photo
+    }
+    return render(request, 'photo/photo_view.html', content)
