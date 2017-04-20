6dc36d21ee5cfd61a8dc6d4ae09c21dc1f6dc8e4
Sergey Lagovskiy <slagovskiy@gmail.com>
2017-04-20 09:59:36 +0700

    device model

--- src/odyssey/photo/models.py
+++ src/odyssey/photo/models.py
@@ -74,6 +74,111 @@ class Tag(models.Model):
         verbose_name_plural = 'Tags'
 
 
+class DeviceType(models.Model):
+    slug = models.SlugField(
+        unique=True
+    )
+    name = models.CharField(
+        max_length=255,
+        default=''
+    )
+    show_in_menu = models.BooleanField(
+        default=True
+    )
+    deleted = models.BooleanField(
+        default=False
+    )
+
+    def __str__(self):
+        return '<DeviceType %s>' % self.name
+
+    @staticmethod
+    def exist(slug=None):
+        if slug is None:
+            return False
+        else:
+            if DeviceType.objects.filter(slug=slug).first() is None:
+                return False
+            else:
+                return True
+
+    class Meta:
+        ordering = ['name',]
+        verbose_name = 'Device type'
+        verbose_name_plural = 'Device types'
+
+
+class Device(models.Model):
+    def image_path(instance, filename):
+        if instance.uid == None:
+            instance.uid = str(uuid.uuid1())
+        ext = filename.split('.')[-1]
+        filename = '{}.{}'.format(str(uuid.uuid1()), ext)
+        return os.path.join(os.path.join('device', instance.uid), filename)
+
+    slug = models.SlugField(
+        unique=True
+    )
+    name = models.CharField(
+        max_length=255,
+        default=''
+    )
+    device_type = models.ForeignKey(
+        'DeviceType'
+    )
+    deleted = models.BooleanField(
+        default=False
+    )
+    image = models.ImageField(
+        'Image',
+        default='',
+        upload_to=image_path
+    )
+    content = models.TextField(
+        default=''
+    )
+
+    def __str__(self):
+        return '<Device %s>' % self.name
+
+    @staticmethod
+    def exist(slug=None):
+        if slug is None:
+            return False
+        else:
+            if Device.objects.filter(slug=slug).first() is None:
+                return False
+            else:
+                return True
+
+    def image_preview(self):
+        if self.image:
+            return '<img src="%s?w=300" border="0"/>' % self.image.url
+        else:
+            return ''
+
+    image_preview.short_description = 'Image preview'
+    image_preview.allow_tags = True
+
+    def image_admin_preview(self):
+        if self.image:
+            preview = '<img src="%s?s=24" border="0"/>' % self.image.url
+            return preview
+        else:
+            return ''
+
+    image_admin_preview.short_description = 'Image'
+    image_admin_preview.allow_tags = True
+
+    def visitor_count(self):
+        return visitor_count(reverse('photo_item', args=[self.slug]))
+
+    class Meta:
+        ordering = ['name',]
+        verbose_name = 'Device'
+        verbose_name_plural = 'Devices'
+
+
 class Photo(models.Model):
     def image_path(instance, filename):
         if instance.uid == None:
