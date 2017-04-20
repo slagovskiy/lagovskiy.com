e4c3efa6dbb07f5d6f296230486b5a019a56270d
Sergey Lagovskiy <slagovskiy@gmail.com>
2017-04-20 13:02:19 +0700

    by tag, by album

--- src/odyssey/photo/views.py
+++ src/odyssey/photo/views.py
@@ -21,14 +21,38 @@ def photo_view(request):
     return render(request, 'photo/index.html', content)
 
 
-def photo_by_album(request):
-    content = []
-    return  render(request, 'photo/index.html', content)
+def photo_by_album(request, slug=''):
+    page = int(request.GET.get('page', 1))
+    album = Album.objects.filter(slug=slug).first()
+    photos = album.photo_set.filter(status=Photo.PUBLISHED_STATUS).order_by('published')
+    paginator = Paginator(photos, PAGE_SIZE)
+    if page <= 0:
+        page = 1
+        if page > paginator.num_pages:
+            page = paginator.num_pages
+
+    content = {
+        'photos': paginator.page(page),
+        'active_album': album
+    }
+    return render(request, 'photo/index.html', content)
 
 
-def photo_by_tag(request):
-    content = []
-    return  render(request, 'photo/index.html', content)
+def photo_by_tag(request, slug=''):
+    page = int(request.GET.get('page', 1))
+    tag = Tag.objects.filter(slug=slug).first()
+    photos = tag.photo_set.filter(status=Photo.PUBLISHED_STATUS).order_by('published')
+    paginator = Paginator(photos, PAGE_SIZE)
+    if page <= 0:
+        page = 1
+        if page > paginator.num_pages:
+            page = paginator.num_pages
+
+    content = {
+        'photos': paginator.page(page),
+        'active_phototag': tag
+    }
+    return render(request, 'photo/index.html', content)
 
 
 def photo_item_view(request, slug=''):
