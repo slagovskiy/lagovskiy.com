b80ca21df3657cecf60ea8d3d8945bbadb748ef9
Sergey Lagovskiy <slagovskiy@gmail.com>
2017-04-30 12:25:49 +0700

    notify

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,15 +1,5 @@
 /* lagovskiy.com */
 
-function notice(color, message){
-    new jBox('Notice', {
-        content: message,
-        color: color,
-        animation:{open:'slide:bottom',close:'slide:left'},
-        attributes:{x:'left',y:'bottom'},
-        theme:'NoticeBorder',
-    });
-}
-
 function isValidEmailAddress(emailAddress) {
     var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
     return pattern.test(emailAddress);
@@ -44,7 +34,7 @@ function sendComment(){
             var data_mes = data.split(':')[0];
             var data_code = data.split(':')[1];
             if(data_mes=='ok') {
-                notice('green', 'saved');
+                $.notify('saved', 'success');
                 var id = $('#comment-parent').val();
                 $.ajax({
                     url: '/blog/comment/' + data_code
@@ -56,11 +46,11 @@ function sendComment(){
                     });
             }
             else {
-                notice('red', data_mes);
+                $.notify(data_mes, "warn");
             }
         })
         .fail(function(){
-            notice('red', 'data transfer error');
+            $.notify('data transfer error', 'error');
         });
 }
 
