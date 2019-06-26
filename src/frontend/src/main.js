a1b4dc5bf5befaff8899d8b00206afe146003993
Sergey Lagovskiy <slagovskiy@gmail.com>
2019-06-26 16:28:01 +0700

    Update main.js

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -19,13 +19,13 @@ Vue.use(VueRouter)
 Vue.use(Vuetify, {
     iconfont: 'fa',
     theme: {
-        "primary": "#546e7a", //"#1976D2",
+        "primary":   "#546e7a", //"#1976D2",
         "secondary": "#424242",
-        "accent": "#82B1FF",
-        "error": "#FF5252",
-        "info": "#2196F3",
-        "success": "#4CAF50",
-        "warning": "#FB8C00",
+        "accent":    "#82B1FF",
+        "error":     "#FF5252",
+        "info":      "#2196F3",
+        "success":   "#4CAF50",
+        "warning":   "#FB8C00",
 
     }
 })
